0effb48b148b27c6c83ad841d5086abf881666a2
Leonardo Rébola <leonardorebola@gmail.com>
2020-02-15 17:04:34 -0300

    Create SIM800L_Mega.ino
    SIM800L Serial3 en Arduino Mega

---
+++ SIM800L_Mega/SIM800L_Mega.ino
@@ -0,0 +1,19 @@
+String valueSerial, valueSerial3;
+
+void setup() {
+  Serial.begin(9600); // Establece la velocidad para el Monitor Serie.
+  Serial.println("SIM800L EVB");
+  Serial3.begin(9600); // Establece la velocidad para el sim800l.
+}
+
+void loop() {
+  if(Serial3.available()) // Si bytes disponibles para leer.
+  {
+    Serial.println(Serial3.readString()); // Lee desde el sim800l e imprime por el Monitor Serial.
+  }
+
+  if(Serial.available()) // Si bytes disponibles para leer.
+  {
+    Serial3.println(Serial.readString()); // Envia los comandos AT que lee desde el Monitor Serial al sim800l.
+  }
+}
